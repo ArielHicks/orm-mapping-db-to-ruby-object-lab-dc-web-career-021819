6d2a1818b07204a32402529b68a301223964281d
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-27 19:10:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,13 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
-    student = self.new
-    student.id[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
-  end
+  # create a new Student object given a row from the database
+  student = self.new
+  student.id = row[0]
+  student.name = row[1]
+  student.grade = row[2]
+  student
+end
 
   def self.all
     # retrieve all the rows from the "Students" database
